201fd08fe0d1ae26e94168d97c3a144b5cbaea19
cdgeass <cdgeass97@gmail.com>
2021-03-05 00:28:44 +0800

    三数之和

---
+++ 18.四数之和.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ * @lc app=leetcode.cn id=18 lang=java
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums.length < 4) {
+            return Collections.emptyList();
+        }
+
+        Arrays.sort(nums);
+        int length = nums.length;
+
+        List<List<Integer>> results = new ArrayList<>();
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if (nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        List<Integer> result = new ArrayList<>();
+                        result.add(nums[i]);
+                        result.add(nums[j]);
+                        result.add(nums[left]);
+                        result.add(nums[right]);
+                        results.add(result);
+
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return results;
+    }
+}
+// @lc code=end
+
